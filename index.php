73c0cb0a32405556c9b747c1bb44c858383a323e
Eva del Carmen Gonzalez Santiz <L24270005@tuxtla.tecnm.mx>
2026-03-27 22:23:29 -0600

    Agregando inicio de sesion a index

---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+
+if (isset($_GET["expirado"])) {
+    session_unset();
+    session_destroy();
+    $_SESSION = [];
+    unset($_SESSION["usuario"]);
+}
+
+// se asigna el tiempo maximo de sesion para el usuario
+$tiempo_maximo = 30;
+
+// 🔒 Cerrar sesión manual
+if (isset($_GET["logout"])) {
+    session_unset();
+    session_destroy();
+    header("Location: ?");
+    exit();
+}
+
+// se verifica cuando se expira la sesion
+if (isset($_SESSION["tiempo_inicio"])) {
+    $tiempo_actual = time();
+    $tiempo_transcurrido = $tiempo_actual - $_SESSION["tiempo_inicio"];
+
+    if ($tiempo_transcurrido > $tiempo_maximo) {
+        session_unset();
+        session_destroy();
+        header("Location: ?expirado=true");
+        exit();
+    }
+}
+//declaracion de usuari y contraseñas simulados
+$usuarioCorrecto = "admin";
+$passwordCorrecto = "1234";
+
+// se procesa el inicio de sesion
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["usuario"];
+    $password = $_POST["password"];
+
+    if ($usuario == $usuarioCorrecto && $password == $passwordCorrecto) {
+        $_SESSION["usuario"] = $usuario;
+        $_SESSION["tiempo_inicio"] = time();
+    } else {
+        $error = "Usuario o contraseña incorrectos";
+    }
+}
+
+// extra
+$tiempoActivo = 0;
+if (isset($_SESSION["tiempo_inicio"])) {
+    $tiempoActivo = time() - $_SESSION["tiempo_inicio"];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi sitio web</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+
+<?php include("includes/header.php"); ?>
+<?php include("includes/menu.php"); ?>
+
+<h1>Bienvenido</h1>
+
+<?php if (isset($_GET["expirado"])) { ?>
+    <p style="color:red;">Tu sesión ha expirado</p>
+<?php } ?>
+
+<?php if (!isset($_SESSION["usuario"])) { ?>
+
+    <!-- SE CREA EL FORMULARIO -->
+    <form method="post" class="form-login">
+        <label>Usuario:</label>
+        <input type="text" name="usuario" required><br><br>
+
+        <label>Contraseña:</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Iniciar sesión</button>
+    </form>
+
+    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+
+<?php } else { ?>
+
+    <!--MENSAJES CUANDO EL USUARIO INICIO SESION -->
+    <h2>Hola, <?php echo $_SESSION["usuario"]; ?></h2>
+
+    <div class="contador-box">
+        <p>Restante: <span id="restante"></span></p>
+    </div>
+
+    <a href="?logout=true" class="logout">Cerrar sesión</a>
+
+    <!-- SCRIPT PARA LA CUENTA REGRESIVA-->
+    <script>
+        let tiempo = <?php echo $tiempoActivo; ?>;
+        let maximo = <?php echo $tiempo_maximo; ?>;
+
+        let intervalo = setInterval(actualizarTiempo, 1000);
+
+        function actualizarTiempo() {
+            //document.getElementById("contador").innerText = tiempo;
+            document.getElementById("restante").innerText = maximo - tiempo;
+
+            tiempo++;
+
+            // CUANDO YA PASO EL TIEMPO, SE HACE UN CIERRE DEFINITIVO
+            if (tiempo > maximo) {
+                clearInterval(intervalo);
+                //alert("Sesión expirada");
+                window.location.href = "?expirado=true";
+            }
+        }
+    //    setInterval(actualizarTiempo, 1000);
+    </script>
+
+<?php } ?>
+
+<?php include("includes/footer.php"); ?>
+
+</body>
+</html>
